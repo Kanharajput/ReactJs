2c826642e4cef45bfd79249f5383a5c8ac054a49
Kanha Tomar <kanharajput91@gmail.com>
2023-08-13 12:11:56 +0530

    delete a blog

--- src/BlogDetail.js
+++ src/BlogDetail.js
@@ -1,9 +1,20 @@
 import {useParams} from 'react-router-dom';
 import useFetch from './useFetch.js';
+import {useNavigate} from 'react-router-dom'
 
 const BlogDetail = () => {
     const { id } = useParams();
     const { data: blog, isPending, isResNotFound } = useFetch('http://localhost:8000/blogs/' + id)
+    const navigate = useNavigate();
+
+    const handleDelete = () =>{
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: 'DELETE'
+        }).then(()=> {
+            navigate('/list');
+        })
+    }
+
     return (
         <div className="blog">
             {isPending && <div>Loading.....</div>}
@@ -14,6 +25,7 @@ const BlogDetail = () => {
                     <h2>id - {blog.id}</h2>
                     <h2>{blog.title}</h2>
                     <p>{blog.body}</p>
+                    <button onClick={handleDelete}>Delete</button>
                 </article>
             )}
         </div>
